15f35def9c78f44b15693e15e2458a75ae8a22f8
marbiru <code@uribram.com>
2015-05-03 00:30:47 +0300

    Dummy website array initialised
    Eventually the quiz will need to pull all its elements from an array
    where the info lives; dumb

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var buzzfeed_array = [
+"Buzzfeed",
+"20th",
+];
+
+var websites_array = [
+buzzfeed_array,
+"New York Times",
+];
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // You've GOT to learn how to do this in jQuery
 
-document.getElementById("left_hand_side").innerHTML = "Option 1";
+document.getElementById("left_hand_side").innerHTML = websites_array[0][0];
 
 document.getElementById("right_hand_side").innerHTML = "Option 2";
