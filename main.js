1b1ce1dd94278baee8908311224864c1e974429d
marbiru <code@uribram.com>
2015-05-02 23:52:41 +0300

    Initialising Main.js
    The two options now determined in javascript -- dummy text input for now

---
+++ main.js
@@ -0,0 +1,5 @@
+// You've GOT to learn how to do this in jQuery
+
+document.getElementById("left_hand_side").innerHTML = "Option 1";
+
+document.getElementById("right_hand_side").innerHTML = "Option 2";
