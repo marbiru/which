b6653ef0c96fddd7f5fe98ce60ca05f10b7b6346
marbiru <code@uribram.com>
2015-05-03 09:30:07 +0300

    Dummy answer_evaluation working
    if/else statement determines which number is bigger

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-// You've GOT to learn how to do this in jQuery TK or do you? document.getElementById might just be the right way to do it TK UPDATE: have deleted getElementById, not even sure what's happening now.
+// this is placeholder, will need to be properly determined
 
-left_hand_side.innerHTML = websites_array[0][0];
-
-//document.getElementById("left_hand_side").innerHTML = websites_array[0][0];
-
-right_hand_side.innerHTML = websites_array[1][0];
-
-// Logic determining which website is actually bigger
-
-// A "Right" or "Wrong" statement. var answer_evaluation with if-then logic 
-
-//var answer_evaluation = "Right/Wrong!";
-
-answer_evaluation.innerHTML = "Right/Wrong!";
-
-
-// this is placeholder, will need to be properly randomised
 var left_website = websites_array[0];
 
 var right_website = websites_array[1]; 
 
-//names of websites
+// You've GOT to learn how to do this in jQuery TK or do you? document.getElementById might just be the right way to do it TK UPDATE: have deleted getElementById, not even sure what's happening now.
+
+left_hand_side.innerHTML = left_website[0];
+
+right_hand_side.innerHTML = right_website[0];
+
+// dummy for now
+
+var user_chose_site = right_website;
+
+var rejected_site = left_website;
+
+// did the user get the answer right?
+
+if (user_chose_site[1] < rejected_site[1]) {
+	answer_evaluation.innerHTML = "That's right!"
+} else {
+	answer_evaluation.innerHTML = "Wrong!"
+}
+
+//names of websites in answer
 
 left_website_name.innerHTML = left_website[0];
 
 right_website_name.innerHTML = right_website[0];
 
-//ranks of websites
+//ranks of websites in answer
 
 left_website_rank.innerHTML = left_website[1];
 
