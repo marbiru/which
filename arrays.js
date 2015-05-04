ae7a493993707c20327547a9c137272cc427df2f
marbiru <code@uribram.com>
2015-05-04 12:04:45 +0300

    Added more media co's

--- arrays.js
+++ arrays.js
@@ -1,11 +1,25 @@
+// new media
+
 var buzzfeed_array = [
 	"Buzzfeed",
 	103,
 ];
 
+var vice_array = [
+	"Vice",
+	214,
+]
+
+// old media
+
 var new_york_times_array = [
 	"New York Times",
 	102,
+];
+
+var guardian_array = [
+	"The Guardian",
+	153,
 ];
 
 var websites_array = [
