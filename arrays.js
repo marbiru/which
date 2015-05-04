722605084e01f0bb99ea2f0ba94080f813045349
marbiru <code@uribram.com>
2015-05-04 12:34:47 +0300

    Randomly selected outlet for comparison

--- arrays.js
+++ arrays.js
@@ -10,6 +10,15 @@ var vice_array = [
 	214,
 ]
 
+var new_media_array = [
+	buzzfeed_array,
+	vice_array,
+]
+
+// maybe define a "random element" function to save time
+
+var new_media = new_media_array[Math.floor(Math.random() * new_media_array.length)];
+
 // old media
 
 var new_york_times_array = [
@@ -22,7 +31,14 @@ var guardian_array = [
 	153,
 ];
 
-var websites_array = [
-	buzzfeed_array,
+var old_media_array = [
 	new_york_times_array,
+	guardian_array,
+]
+
+var old_media = old_media_array[Math.floor(Math.random() * old_media_array.length)];
+
+var websites_array = [
+	new_media,
+	old_media,
 ];
