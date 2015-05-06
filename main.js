a27526d2cdfcc2888f5260fae87fdc95db958c53
marbiru <code@uribram.com>
2015-05-06 14:04:24 +0100

    Website buttons work
    Actual evaluation of which site is bigger

--- main.js
+++ main.js
@@ -4,37 +4,40 @@ var left_website = websites_array[0];
 
 var right_website = websites_array[1]; 
 
-// You've GOT to learn how to do this in jQuery TK or do you? document.getElementById might just be the right way to do it TK UPDATE: have deleted getElementById, not even sure what's happening now.
-
 left_hand_side.innerHTML = left_website[0];
 
 right_hand_side.innerHTML = right_website[0];
 
-// dummy for now
+$('#left_hand_side').click(function(){
+    window.selected_site = left_website;
+    window.rejected_site = right_website;
+    $("#answer_evaluation").text(print_answer());
+});	
 
-var user_chose_site = right_website;
+$('#right_hand_side').click(function(){
+    window.selected_site = right_website;
+    window.rejected_site = left_website;
+    $("#answer_evaluation").text(print_answer());
+});
 
-var rejected_site = left_website;
-
-// did the user get the answer right?
-
-if (user_chose_site[1] < rejected_site[1]) {
-	answer_evaluation.innerHTML = "That's right!"
-} else {
-	answer_evaluation.innerHTML = "Wrong!"
+function print_answer(){
+	if (selected_site[1] < rejected_site[1]) {
+		var answer_eval_text = "That's right!";
+	} else {
+		var answer_eval_text = "Wrong!";
+	};
+	answer_evaluation.innerHTML = answer_eval_text;
+	left_website_name.innerHTML = left_website[0] + " is";
+	right_website_name.innerHTML = right_website[0] + " is";
+	left_website_rank.innerHTML = "the No. " + left_website[1] + " biggest website in the world.";
+	right_website_rank.innerHTML = "the No. " + right_website[1] + " biggest website in the world.";
 }
 
-//names of websites in answer
+$('#right_hand_side').click(function() {
+    var user_chose_site = right_website;
+    var rejected_site = left_website;
+});
 
-left_website_name.innerHTML = left_website[0];
-
-right_website_name.innerHTML = right_website[0];
-
-//ranks of websites in answer
-
-left_website_rank.innerHTML = left_website[1];
-
-right_website_rank.innerHTML = right_website[1];
 
 // Reload page on button click
 
