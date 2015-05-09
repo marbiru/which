504ac32b836b12d7d84cbc17672f9f669891bdd0
marbiru <code@uribram.com>
2015-05-09 14:31:02 +0100

    Condensed print_options into single function
    much better design! DRY: don't repeat yourself

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 $(function(){
-  $( "#left_hand_side" ).text(print_left_hand_side());
-  $( "#right_hand_side" ).text(print_right_hand_side());
+  $( "#left_hand_side, #right_hand_side" ).text(print_options());
   $( "#answer" ).on( "dialogclose", function() {
-    $( "#left_hand_side" ).text(print_left_hand_side());
-    $( "#right_hand_side" ).text(print_right_hand_side());      
+    $( "#left_hand_side, #right_hand_side" ).text(print_options());    
   });
 });
 
-function print_left_hand_side(){
+function print_options(){
   current_pair = select_random(ultimate_array);
   left_array = current_pair[0];
   left_website = select_random(left_array);
   left_hand_side.innerHTML = left_website[0];
-};
-
-function print_right_hand_side(){
   right_array = current_pair[1];
   right_website = select_random(right_array);
   right_hand_side.innerHTML = right_website[0];
